a4f559bef75d64e69e5fbf1ab28407ab3fa655fd
Sidda Chalamalasetty <tosidda@gmail.com>
2020-05-13 15:57:40 -0400

    relative :)

--- src/components/pageElements/Code.js
+++ src/components/pageElements/Code.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from 'prism-react-renderer/themes/nightOwl'
 import styled from 'styled-components'
+import { copyToClipboard } from '../../utils/copy-to-clipboard';
 
 const Pre = styled.pre`
+    position: relative;
     text-align: left;
     margin: 1em 0;
     padding: 0.5em;
@@ -24,11 +26,27 @@ const LineNo = styled.span`
     opacity: 0.3;
 `
 
+const CopyCode = styled.button`
+    position: absolute;
+    right: 0.25rem;
+    border-radius: 3px;
+    margin: 0.25em;
+    opacity: 0.3;
+    &:hover {
+        opacity: 1;
+        transition: 0.3s opacity ease-in-out;
+    }
+`
+
 export const Code = ({codeString, language, ...props}) => {
+    const handleClick = () => {
+        copyToClipboard(codeString)
+    }
     return (
         <Highlight {...defaultProps} code={codeString} theme={theme} language={language}>
     {({ className, style, tokens, getLineProps, getTokenProps }) => (
       <Pre className={className} style={style}>
+        <CopyCode onClick={handleClick}>Copy the Code</CopyCode>
         {tokens.map((line, i) => (
           <div {...getLineProps({ line, key: i })}>
             <LineNo>{i + 1} </LineNo>

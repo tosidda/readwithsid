604221fb83940b13218862974c85f927afa4ba84
Sidda Chalamalasetty <tosidda@gmail.com>
2020-05-16 15:09:28 -0400

    made sitemeta var

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
+const siteMetadata = {
+  title: `READ WITH SID`,
+  description: `CODE AND READ AMD DO MATH AND OTHER NERDY STUFF`,
+  image: `/defualt-site-image.jpg`,
+  siteUrl: `https://readwithsid.com`,
+  siteLanguage: `en-US`,
+  siteLocale: `en_us`,
+  twitterUsername: `@readwithsid`,
+  authorName: `Sidda Chalamalasetty`,
+} 
+
 module.exports = {
-  siteMetadata: {
-    title: `READ WITH SID`,
-    description: `CODE AND READ AMD DO MATH AND OTHER NERDY STUFF`
-  },
+  siteMetadata: siteMetadata,
   plugins: [
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,

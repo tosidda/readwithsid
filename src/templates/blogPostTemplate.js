a02f1d8d7723a4d08c42d1e15a2f62272fbf633a
Sidda Chalamalasetty <tosidda@gmail.com>
2020-05-16 15:10:39 -0400

    MADE SEO CRAP

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -3,12 +3,30 @@ import {MDXRenderer} from 'gatsby-plugin-mdx'
 import {graphql, Link} from 'gatsby'
 import {Layout} from '../components/Layout'
 import Dump from '../components/Dump'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
+import SEO from 'react-seo-component'
 
 export default ({data, pageContext}) => {
-    const {frontmatter, body} = data.mdx
+    const {frontmatter, body, fields, excerpt} = data.mdx
     const {previous, next} = pageContext
+    const {siteLanguage, siteLocale, twitterUsername, authorName, siteUrl, image} = useSiteMetadata()
+    const {title, cover, date} = frontmatter
     return (
         <Layout>
+            <Dump
+                title={title}
+                description={excerpt}
+                image={`${siteUrl}${image}`}
+                image={cover === null ? `${siteUrl}${image}` : `${siteUrl}${cover.publicURL}`}
+                pathname={`${siteUrl}${fields.slug}`}
+                siteLanguage={siteLanguage}
+                siteLocale={siteLocale}
+                twitterUsername={twitterUsername}
+                author={authorName}
+                article={true}
+                publishedDate={date}
+                modifiedDate={new Date(Date.now()).toISOString()}
+            />
             <h1>{frontmatter.title}</h1>
             <p>{frontmatter.date}</p>
             <MDXRenderer>{body}</MDXRenderer>
@@ -35,13 +53,20 @@ export default ({data, pageContext}) => {
 }
 
 export const query = graphql`
-query PostBySlg($slug:String){
-    mdx(fields: {slug: {eq: $slug}}) {
+query PostBySlug($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
       frontmatter {
-        date 
         title
+        date(formatString: "YYYY MMMM Do")
+        cover {
+          publicURL
+        }
       }
       body
+      excerpt
+      fields {
+        slug
+      }
     }
   }
 `
